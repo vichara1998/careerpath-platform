1afd759abbceb22374743dc6562f2fa2af73e463
Vichara Tharkana <vichara0115@gmail.com>
2026-05-27 09:57:29 +0530

    Spring Boot Initilization

---
+++ careerpath-backend/src/main/java/lk/careerpath/careerpath_backend/CareerpathBackendApplication.java
@@ -0,0 +1,15 @@
+package lk.careerpath.careerpath_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerpathBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CareerpathBackendApplication.class, args);
+
+	}
+
+}
---
+++ careerpath-backend/src/test/java/lk/careerpath/careerpath_backend/CareerpathBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.careerpath.careerpath_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerpathBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
