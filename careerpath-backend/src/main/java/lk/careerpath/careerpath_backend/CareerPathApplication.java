14c729921d6f867df501d83399fb487a9af7b375
Vichara Tharkana <vichara0115@gmail.com>
2026-05-27 10:42:51 +0530

    Enable async processing and scheduling support

--- careerpath-backend/src/main/java/lk/careerpath/careerpath_backend/CareerPathApplication.java
+++ careerpath-backend/src/main/java/lk/careerpath/careerpath_backend/CareerPathApplication.java
@@ -2,14 +2,14 @@ package lk.careerpath.careerpath_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class CareerPathApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(CareerPathApplication.class, args);
-
 	}
-
 }
